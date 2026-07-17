09d1f29c75cb69a920663b3f3a22c9b5ba6583c2
Yuktha Naik <yuktha.naik@clarivate.com>
2026-07-17 10:55:56 +0530

    Added customer and reservation entities and repositories

---
+++ src/main/java/org/capstonegrp8/restaurant_management_system/repository/ReservationRepository.java
@@ -0,0 +1,10 @@
+package org.capstonegrp8.restaurant_management_system.repository;
+
+import org.capstonegrp8.restaurant_management_system.model.Reservation;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ReservationRepository extends JpaRepository<Reservation, Long> {
+
+}
