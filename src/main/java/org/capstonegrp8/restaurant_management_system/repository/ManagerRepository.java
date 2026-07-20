316607cb0cc6525f2cd4ac96a2899b10bb8d4e5f
abhin5821 <abhinvaidya5821.com>
2026-07-20 21:22:06 +0530

    model ready for payment and table. ALso, done minor dependency fixes, path ect

--- src/main/java/org/capstonegrp8/restaurant_management_system/repository/ManagerRepository.java
+++ src/main/java/org/capstonegrp8/restaurant_management_system/repository/ManagerRepository.java
@@ -1,6 +1,5 @@
 package org.capstonegrp8.restaurant_management_system.repository;
 
-import org.capstonegrp8.restaurant_management_system.model.Manager;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface ManagerRepository<Manager> extends JpaRepository<Manager, Long> {
